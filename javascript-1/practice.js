e328881725db22cc3c225e57cccb9bfc93d56850
JaidenSalixB <divine.aligned.mind@gmail.com>
2019-05-16 16:51:01 -0600

    unfinished-skill-checkpoint

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -3,11 +3,12 @@
 // Create a variable called 'myName' that's value is your name
 
 // Code here
+var myName = 'Jaiden'
 
 //////////////////PROBLEM 2////////////////////
 
 // Create a variable called 'faveNum' that's value is your favorite number
-
+var faveNum= 3
 //Code here
 
 //////////////////PROBLEM 3////////////////////
@@ -15,36 +16,62 @@
 // Create a variable called 'lovesCode' and set it to true
 
 //Code here
-
+var lovesCode= true
 //////////////////PROBLEM 4////////////////////
 
 // Create a function called 'sum' that takes in two parameters, num1 and num2, and returns the sum of those two parameters.
 
 //Code here
+function sum (num1,num2){
+	return num1+num2
+}
+sum(1,5)
 
 //////////////////PROBLEM 5////////////////////
 
 // Create a function called 'lovesCodeChecker' that takes in a single parameter called x. Check to see if x is equal to true. If it is, return true as a boolean. If x does not equal true, return false as a boolean
 
 //Code here
-
+let x= true
+function lovesCodeChecker(x){
+	if(x===true)
+	{return true}
+	else {return false}
+}
 //////////////////PROBLEM 6////////////////////
 
-// Create a function called 'oddChecker' that takes in one parameter, num. Check to see if the number is even or odd. If it is odd, return 'the number is odd' or return 'the number is even' if it is even.
+// Create a function called 'oddChecker' that takes in one parameter, num. 
+//Check to see if the number is even or odd. If it is odd, return 'the number is odd' or return 'the number is even' if it is even.
+function oddChecker(num){
+    if (num%2 == 0)
+        return 'the number is even';
+    else
+        return 'the number is odd';
+}
+
 
 //Code here
 
+
 //////////////////PROBLEM 7////////////////////
 
-// Create a function called 'iLove' that takes in two parameters, name which will be a string  and love which will be a string of something you love. Have the function take the two parameters and retrun a string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Bryan loves skateboarding"
+// Create a function called 'iLove' that takes in two parameters, name which will be a string  
+//and love which will be a string of something you love. Have the function take the two parameters and retrun a string that says
+// "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Bryan loves skateboarding"
 
 //Code Here
+
+function iLove (name,love){
+	return (name + 'loves ' + love)
+
+}
+iLove('Jaiden ','dance')
 
 //////////////////PROBLEM 8////////////////////
 
 // Create a copy of the faveColors array called 'colorCopy' using slice
 const faveColors = ['red', 'green', 'black']
-
+var colorCopy= faveColors.slice()
 //Code Here
 
 //////////////////PROBLEM 9////////////////////
@@ -52,6 +79,7 @@ const faveColors = ['red', 'green', 'black']
 // Add a fourth color to the end of the 'colorCopy' array using push
 
 //Code Here
+colorCopy.push('blue')
 
 //////////////////PROBLEM 10////////////////////
 
@@ -60,23 +88,45 @@ const numbers = [1, 2, 3, 4, 5]
 
 //Code Here
 
+var middleNums=numbers.slice(1, -1);
+
+
 //////////////////PROBLEM 11////////////////////
 
-// Create an object called 'me' that has these keys with the correct values: firstName, state, age, greeter. firstName will be your name as a string. state will be your current state or providence as a string. age will be your age as a number. greeter will be a method that returns the string 'Hello! My name is NAMEVALUE and I live in STATEVALUE' with the correct values
+// Create an object called 'me' that has these keys with the correct values: 
+// firstName, state, age, greeter. firstName will be your name as a string. state will
+//  be your current state or providence as a string. age will be your age as a number. greeter
+//   will be a method that returns the string 'Hello! My name is NAMEVALUE and I live in STATEVALUE' with the correct values
 
 //Code Here
-
+var me= {firstName:"Jaiden", state: 'Utah', age:22, greeter: function(){return 'Hello! My name is '+ this.firstName + " and I live in " + this.State}}
 //////////////////PROBLEM 12////////////////////
 
-// Create a function called 'bigOrSmall' that takes in one parameter, arr. Create a new array inside of bigOrSmall called 'answers'. Loop over the passed in arr param. If the number is GREATER than 100, push 'big' as a string to the answers array. If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. Return the answers array inside of the function
+// Create a function called 'bigOrSmall' that takes in one parameter, arr. 
+//Create a new array inside of bigOrSmall called 'answers'.
+// Loop over the passed in arr param. If the number is GREATER than 100, push 'big' as a string to the answers array. 
+//If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. Return the answers array inside of the function
 
 // Code here
-
+function bigOrSmall(arr){
+	var answers=[300,400,500,600,700]
+	for(var i=0;arr.length>i;i++)
+	answers.push(arr[i])
+	if ([i]>100)
+	return answers.push('big')
+	else ([i]<=100)
+	return answers.push('small')
+}bigOrSmall()
 //////////////////PROBLEM 13////////////////////
 
-// Create a function called 'arrayReverser' that takes in one parameter, arr. Inside of arrayReverser, create an empty array called 'reversed'. Using a for loop, loop over the passed in array in reverse and add each item to the new reversed array. Finally, return the new reversed array
+// Create a function called 'arrayReverser' 
+// that takes in one parameter, arr. Inside of arrayReverser, 
+// create an empty array called 'reversed'. Using a for loop,
+//  loop over the passed in array in reverse and add each item to the 
+//  new reversed array. Finally, return the new reversed array
 
 //Code Here
+
 
 //////////////////PROBLEM 14 - 18 Setup////////////////////
 
